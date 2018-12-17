d9de2613978f423aef48814620cb290e439a2e42
Alex Tilot <34109200+Nezzely@users.noreply.github.com>
2018-12-16 19:30:43 -0600

    took data into new File

---
+++ Data.js
@@ -0,0 +1,39 @@
+export default {
+  campaignList: [
+    {
+      key: "The Eye of the World",
+      image:
+        "https://s3-us-west-2.amazonaws.com/tabs.web.media/b/7/b7rb/b7rb-square-1536.jpg"
+    },
+    {
+      key: "The Great Hunt",
+      image:
+        "https://s3-us-west-2.amazonaws.com/tabs.web.media/b/7/b7rc/b7rc-square-1536.jpg"
+    },
+    {
+      key: "The Dragon Returns",
+      image:
+        "https://kbimages1-a.akamaihd.net/7aa3ed12-3c4f-4d56-ba37-931708133ff4/1200/1200/False/the-dragon-reborn-7.jpg"
+    },
+    {
+      key: "The Shadow Rising",
+      image:
+        "https://kbimages1-a.akamaihd.net/636adc65-9392-4aaa-8bee-d884df6d1442/1200/1200/False/the-shadow-rising-3.jpg"
+    },
+    {
+      key: "The Fire of Heaven",
+      image:
+        "https://kbimages1-a.akamaihd.net/d045b921-f10c-4ac2-9d08-5a0f9c0a58aa/353/569/90/False/the-fires-of-heaven-4.jpg"
+    },
+    {
+      key: "Lord of Chaos",
+      image:
+        "https://s3-us-west-2.amazonaws.com/tabs.web.media/b/7/b7mo/b7mo-square-1536.jpg"
+    },
+    {
+      key: "A Crown of Swords",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/61R6W9zBrlL._AA300_.jpg"
+    }
+  ]
+};
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -8,11 +8,14 @@ import {
   TouchableOpacity
 } from "react-native";
 import { LinearGradient } from "expo";
+import data from "../Data";
 
 export default class MainScreen extends React.Component {
-  NavCampaignForm = () => {
-    //navigate to the form
-  };
+  constructor(props) {
+    super(props);
+    this.state = { campaignData: data.campaignList };
+  }
+
   NextScreenTemp = itemName => {
     this.props.navigation.navigate("CampaignScreen", {
       campaignName: itemName
@@ -45,43 +48,8 @@ export default class MainScreen extends React.Component {
         style={styles.linearGradient}
       >
         <FlatList
-          data={[
-            {
-              key: "The Eye of the World",
-              image:
-                "https://s3-us-west-2.amazonaws.com/tabs.web.media/b/7/b7rb/b7rb-square-1536.jpg"
-            },
-            {
-              key: "The Great Hunt",
-              image:
-                "https://s3-us-west-2.amazonaws.com/tabs.web.media/b/7/b7rc/b7rc-square-1536.jpg"
-            },
-            {
-              key: "The Dragon Returns",
-              image:
-                "https://kbimages1-a.akamaihd.net/7aa3ed12-3c4f-4d56-ba37-931708133ff4/1200/1200/False/the-dragon-reborn-7.jpg"
-            },
-            {
-              key: "The Shadow Rising",
-              image:
-                "https://kbimages1-a.akamaihd.net/636adc65-9392-4aaa-8bee-d884df6d1442/1200/1200/False/the-shadow-rising-3.jpg"
-            },
-            {
-              key: "The Fire of Heaven",
-              image:
-                "https://kbimages1-a.akamaihd.net/d045b921-f10c-4ac2-9d08-5a0f9c0a58aa/353/569/90/False/the-fires-of-heaven-4.jpg"
-            },
-            {
-              key: "Lord of Chaos",
-              image:
-                "https://s3-us-west-2.amazonaws.com/tabs.web.media/b/7/b7mo/b7mo-square-1536.jpg"
-            },
-            {
-              key: "A Crown of Swords",
-              image:
-                "https://images-na.ssl-images-amazon.com/images/I/61R6W9zBrlL._AA300_.jpg"
-            }
-          ]}
+          style={styles.listView}
+          data={this.state.campaignData}
           renderItem={this.renderItem}
         />
       </LinearGradient>
